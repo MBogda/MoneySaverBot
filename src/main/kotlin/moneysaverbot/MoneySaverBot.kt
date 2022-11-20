b1e033d86651375744716e0f677c3d1208915d2b
Bogdan Markov <bogdick@yandex.ru>
2022-11-20 21:36:01 +0100

    MoneySaverBot - first draft.

---
+++ src/main/kotlin/moneysaverbot/MoneySaverBot.kt
@@ -0,0 +1,12 @@
+package moneysaverbot
+
+import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
+
+class MoneySaverBot {
+
+    private var showOption = ShowOptions.INLINE_KEYBOARD
+
+    suspend fun BehaviourContext.main() {
+
+    }
+}
---
+++ src/main/kotlin/moneysaverbot/ShowOptions.kt
@@ -0,0 +1,6 @@
+package moneysaverbot
+
+enum class ShowOptions {
+    INLINE_KEYBOARD,
+    REPLY_KEYBOARD,
+}
